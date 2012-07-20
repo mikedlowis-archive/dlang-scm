eee167180360cb6b13ac29148409eeb1b2f9d606
Mike D. Lowis <mike@mdlowis.com>
2012-07-20 13:10:10 -0400

    Added code to main for parsing input from a file

--- source/main.scm
+++ source/main.scm
@@ -1,5 +1,14 @@
-(include "loop.scm")
-(declare (uses buf)
-         (uses lexer)
+(declare
+
          (uses parser))
+
+(define (parse-file fname)
+  (define result
+    (dlang/program (dlang/lexer (open-input-file fname))))
+  (print result))
+
+; If we have a file, then parse it
+(if (= 1 (length (command-line-arguments)))
+  (parse-file (car (command-line-arguments)))
+  (print "No input file provided."))
 
